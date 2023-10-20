d58f67377c8eeae30801c00af62d5f7abc5f95da
Alex Marshall <alex.marshall@rfidentikit.com>
2023-10-20 12:25:25 +0100

    Colours working across all pages, page must contain .colour somewhere to use

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const colour = document.querySelector(".colour");
 
+    // Function to get a cookie by name
+    function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length === 2) return parts.pop().split(";").shift();
+    }
+
+    // Retrieve the background colour from the cookie (if it exists)
+    var savedColour = getCookie("background_colour");
+
+    if (savedColour) {
+
+        document.body.style.backgroundColor = savedColour;
+    }
+});
--- JS/hex.js
+++ JS/hex.js
@@ -1,16 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("Hexbtn");
-const color = document.querySelector(".colour");
+const colour = document.querySelector(".colour");
 
 btn.addEventListener("click", function () {
   let hexColour = "#";
   for (let i = 0; i < 6; i++) {
     hexColour += hex[getRandomNumber()];
   }
-  // console.log(hexColour);
 
-  color.textContent = hexColour;
+  colour.textContent = hexColour;
   document.body.style.backgroundColor = hexColour;
+
+  // Set the hexColour as a cookie
+  document.cookie = `background_colour=${hexColour}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
 });
 
 function getRandomNumber() {
