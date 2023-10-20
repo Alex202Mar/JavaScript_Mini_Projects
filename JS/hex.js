2030797141431e32a82f2e12298e7aade50d53cc
Alex Marshall <alex.marshall@rfidentikit.com>
2023-10-20 09:56:47 +0100

    Reverting Changes to JS pre Cookies

--- JS/hex.js
+++ JS/hex.js
@@ -1,9 +1,18 @@
-// Assuming you have a button with ID "generateColourBtn" on your second page
-const generateColourBtn = document.getElementById("generateColourBtn");
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("Hexbtn");
+const color = document.querySelector(".colour");
 
-generateColourBtn.addEventListener("click", function() {
-    const hexColour = generateRandomColour();
+btn.addEventListener("click", function () {
+  let hexColour = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColour += hex[getRandomNumber()];
+  }
+  // console.log(hexColour);
 
-    // Set the hexColour as a cookie
-    document.cookie = `background_Colour=${hexColour}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+  color.textContent = hexColour;
+  document.body.style.backgroundColor = hexColour;
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
